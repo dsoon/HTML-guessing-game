9e0d69db7770e42d617e099500a356c0aa19d99b
Mr Soon <>
2021-05-25 07:07:16 +0000

    Added input tag

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 var secret=(Math.random() * 100).toFixed(0);
 function dosomething() {
+    myinput = document.getElementById("myinput").value;
     names = document.getElementsByClassName("output");
     alert(names);
     for (i =0; i < names.length; i++){
-        names[i].hidden = true;
+        names[i].innerText = myinput;
     }
 } 
 
